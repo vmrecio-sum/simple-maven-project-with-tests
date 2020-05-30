c65b09e5b5ebdc483e5361a287cf91886a23b042
vmrecio-sum <57141392+vmrecio-sum@users.noreply.github.com>
2020-05-30 18:12:07 +0200

    Nueva Prueba

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -33,9 +33,8 @@ class Base {
         double r = Math.random();
         if (r < 0.1) {
             fail("oops");
-        } 
-	else if (r < 0.12) { 
-          throw new AssumptionViolatedException("skipping");
+        } else if (r < 0.12) { 
+            throw new AssumptionViolatedException("skipping");
         }
     }
 
